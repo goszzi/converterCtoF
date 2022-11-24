baa8134507f599e9ece84e7f5d7ca94883a39313
M <106150150+goszzi@users.noreply.github.com>
2022-11-24 14:46:44 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,54 @@
+const converter = document.querySelector('#converter')
+const result = document.querySelector('.result')
+const convBtn = document.querySelector('.conv')
+const resetBtn = document.querySelector('.reset')
+const changeBtn = document.querySelector('.change')
+const one = document.querySelector('.one')
+const two = document.querySelector('.two')
+
+let fahrenheit
+let celsius
+
+const swap = () => {
+	if (one.textContent === '°C') {
+		one.textContent = '°F'
+		two.textContent = '°C'
+		result.textContent = ''
+	} else {
+		one.textContent = '°C'
+		two.textContent = '°F'
+		result.textContent = ''
+	}
+}
+
+const fahrToCel = () => {
+	fahrenheit = converter.value * 1.8 + 32
+	result.textContent = `${converter.value}°C to ${fahrenheit.toFixed(1)}°F`
+	converter.value = ''
+}
+
+const celToFahr = () => {
+	celsius = (converter.value - 32) / 1.8
+	result.textContent = `${converter.value}°F to ${celsius.toFixed(1)}°C`
+	converter.value = ''
+}
+
+const conversion = () => {
+	if (converter.value !== '') {
+		if (one.textContent === '°C') {
+			fahrToCel()
+		} else {
+			celToFahr()
+		}
+	} else {
+		result.textContent = 'Musisz podać jakąś wartość!'
+	}
+}
+
+const reset = () => {
+	converter.value = ''
+	result.textContent = ''
+}
+changeBtn.addEventListener('click', swap)
+convBtn.addEventListener('click', conversion)
+resetBtn.addEventListener('click', reset)
